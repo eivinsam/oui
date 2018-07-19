4c9a41e5c4c5a5a4bcf91c39d9883a1cd521f70e
Eivind Samuelsen <eivind.samuelsen@gmail.com>
2018-07-19 16:01:36 +0200

    Added utf8len text utility function

--- include/oui_text.h
+++ include/oui_text.h
@@ -10,6 +10,17 @@ namespace oui
 {
 	int popCodepoint(std::string_view& text);
 	std::string utf8(int code);
+	inline constexpr int utf8len(unsigned char ch)
+	{
+		switch (ch >> 4)
+		{
+		case 0xf: return 4;
+		case 0xe: return 3;
+		case 0xd: case 0xc: return 2;
+		default:
+			return (ch >> 7) ^ 1;
+		}
+	}
 
 	class Font
 	{
